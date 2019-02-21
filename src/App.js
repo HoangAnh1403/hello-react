dc44d5c543bb95b7b0473b0e7497eb871ba85c0d
Jane Hoang Anh <van.a@shopstack.asia>
2019-02-21 09:11:47 +0700

    work home work

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Chào mừng đến với khóa học ReactJS
+          </p>
+         
+        </header>
+         <div className="App-content">Để bắt đầu học vui lòng truy cập <code>src/App.js</code> để chỉnh sửa và lưu lại</div>
+      </div>
+    );
+  }
+}
+
+export default App;
